2770cf172378ce4c8f87aae63b6fb9b2d71a59be
Oleksii Skopiuk <oskop@softserveinc.com>
2022-02-15 17:57:07 +0200

    ïnit commit

---
+++ js/main.js
@@ -0,0 +1,218 @@
+const { Model, Collection, View, Router, history } = Backbone;
+//==================================================
+//====MODELS+=======================================
+//==================================================
+const Vehicle = Model.extend({
+  idAttribute: 'regNumber',
+  urlRoot: '/api/vehicles/',
+
+  start: function () {
+    console.log('Vehicle started...');
+  },
+
+  validate: function (attrs) {
+    if (!attrs.registrationNumber) {
+      return 'Please provide registration number';
+    }
+  },
+
+  checkValidation: function () {
+    const isValid = this.isValid();
+
+    if (!isValid) {
+      return console.warn(this.validationError);
+    }
+
+    console.log(`The car with reg number ${this.get('regNumber')} is valid. `);
+  },
+});
+
+const Car = Vehicle.extend({
+  defaults: {
+    regNumber: null,
+    colour: 'white',
+  },
+
+  start: function () {
+    console.log(`Car with reg num ${this.get('regNumber')} has started...`);
+  },
+});
+
+const Boat = Vehicle.extend({
+  defaults: {
+    regNumber: null,
+    material: 'wood',
+  },
+  start: function () {
+    console.log(`Boat with reg num ${this.get('regNumber')} has sailed...`);
+  },
+});
+//==================================================
+//====COLLECTION====================================
+//==================================================
+const Cars = Collection.extend({ model: Car });
+const Boats = Collection.extend({ model: Boat });
+//==================================================
+//====VIEWS++=======================================
+//==================================================
+
+const CarView = View.extend({
+  tagName: 'li',
+  className: 'vehicle',
+  attributes: {
+    'data-colour': '',
+  },
+
+  render: function () {
+    const source = $('#carTemplate').html();
+    const template = _.template(source);
+    const html = template(this.model.toJSON());
+
+    this.$el.html(html);
+    this.$el.attr('id', this.model.cid);
+
+    return this;
+  },
+});
+
+const BoatView = View.extend({
+  tagName: 'li',
+  className: 'vehicle',
+  attributes: {
+    'data-material': '',
+  },
+
+  render: function () {
+    const source = $('#boatTemplate');
+    const template = _.template(source);
+    const html = template(this.model.toJSON());
+
+    this.$el.html(html);
+    this.$el.attr('id', this.model.cid);
+
+    return this;
+  },
+});
+
+const CarsView = View.extend({
+  el: $('#carsContainer'),
+
+  render: function () {
+    const self = this;
+    console.log('this.model', this.model);
+
+    this.model.each(function (car) {
+      const carView = new CarView({ model: car });
+
+      self.$el.append(carView.render().$el);
+    });
+  },
+});
+
+const HomePageView = View.extend({
+  el: '#container',
+  render: function () {
+    this.$el.html('HOME VIEW');
+
+    return this;
+  },
+});
+
+// const CarsPageView = View.extend({
+//   el: '#container',
+
+//   initialize: function () {
+//     const carsView = new CarsView({ el: '#carsContainer', model: cars });
+
+//     console.log('carsView', carsView);
+
+//     carsView.render();
+//     this.render();
+//   },
+
+//   render: function () {
+//     const source = $('#carsPageTemplate').html();
+//     const template = _.template(source);
+
+//     this.$el.html(template());
+
+//     return this;
+//   },
+// });
+
+const BoatsPageView = View.extend({
+  el: '#container',
+  render: function () {
+    this.$el.html('BOATS VIEW');
+
+    return this;
+  },
+});
+
+const DefaultPageView = View.extend({
+  el: '#container',
+  render: function () {
+    this.$el.html('PAGE NOT FOUND');
+
+    return this;
+  },
+});
+
+const NavView = View.extend({
+  events: {
+    click: 'onLinkClick',
+  },
+
+  onLinkClick: function (event) {
+    const $li = $(event.target);
+    appRouter.navigate($li.attr('data-url'), { trigger: true });
+  },
+});
+
+//==================================================
+//====ROUTER+=======================================
+//==================================================
+const AppRouter = Router.extend({
+  routes: {
+    '': 'viewHome',
+    cars: 'viewCars',
+    boats: 'viewBoats',
+    '*other': 'viewDefault',
+  },
+
+  viewHome: function () {
+    // const view = new HomePageView();
+    const view = new CarsView();
+    view.render();
+  },
+  viewCars: function () {
+    const view = new CarsPageView();
+    view.render();
+  },
+  viewBoats: function () {
+    const view = new BoatsPageView();
+    view.render();
+  },
+  viewDefault: function () {
+    const view = new DefaultPageView();
+    view.render();
+  },
+});
+
+//==================================================
+//====INITIATE======================================
+//==================================================
+const cars = new Cars([
+  new Car({ regNumber: 'XLI887', colour: 'Blue' }),
+  new Car({ regNumber: 'ZNP123', colour: 'Blue' }),
+  new Car({ regNumber: 'XUV456', colour: 'Gray' }),
+]);
+
+const appRouter = new AppRouter();
+const navView = new NavView({ el: '#nav' });
+const carsView = new CarsView({ model: cars });
+
+history.start({ pushState: true });
+
+navView.render();
+carsView.render();
